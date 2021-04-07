7217fb3ff717ecddfea7467c973742b730adf147
Vladimir_Novikov <vovasnew@mail.ru>
2021-04-07 13:50:59 +0500

    multiplication_table

---
+++ lesson_1.py
@@ -0,0 +1,27 @@
+def multiplication_table(a, b):
+    for i in range(1, a + 1):
+        for j in range(1, b + 1):
+            print(f"{i} x {j} = {i * j}")
+
+
+def num_input():
+    exit = False
+    while not exit:
+        numbers = input("Введите 2 целых числа через пробел (или q для выхода): ").split()[:2]
+        if numbers[0] == "q" or numbers[0] == "й":
+            exit = True
+            break
+        if len(numbers) < 2:
+            print("Нужно ввести 2 числа")
+            continue
+        try:
+            int(numbers[0])
+            int(numbers[1])
+        except ValueError:
+            print("Нужно вводить целые числа")
+            continue
+
+        multiplication_table(int(numbers[0]), int(numbers[1]))
+
+
+num_input()
